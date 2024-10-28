e50d4967ee915a0ecea5ac62467120cfab1f4cf2
Emil Latypov <emillatypov9335@gmail.com>
2024-10-28 21:13:20 +0300

    Establish encoded connection via TCP Stream

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 mod encoder;
 mod gateway;
 
+use std::io::{Read, Write};
 use std::time::Duration;
-use std::{env, io, thread};
+use std::{env, thread};
 
 use encoder::*;
-use mio::net::UdpSocket;
+use mio::net::{TcpStream, UdpSocket};
 use mio::{Events, Interest, Poll, Token};
-use std::net::SocketAddr;
+use std::net::{SocketAddr, TcpListener as TcpListenerStd, TcpStream as TcpStreamStd};
 
 fn prepare_client(
     encoded_addr: SocketAddr,
     decoded_addr: SocketAddr,
-    encoder: &Box<dyn Encoder>,
-) -> std::io::Result<(UdpSocket, UdpSocket)> {
+) -> std::io::Result<(TcpStream, UdpSocket)> {
     // In client mode, decoded gateway is connection to local client (listen to it).
     // Encoded gateway is connection to remote server (connect to it).
 
@@ -55,31 +55,16 @@ fn prepare_client(
         local_client
     );
 
-    // Meanwhile encoded gateway is explicitly associated with remote socket
-    // (we can let the system to set local credentials)
-    let encoded_gateway = UdpSocket::bind("0.0.0.0:0".parse().unwrap())?;
-    encoded_gateway.connect(encoded_addr)?;
+    // TODO: Documentation
+    let encoded_gateway = TcpStreamStd::connect(encoded_addr)?;
 
-    // Send HELLO message to the server in order to immediatelly create association for server
-    let enc_msg = encoder.encode("HELLO FROM YOKI CLIENT".as_bytes().to_vec());
-
-    match encoded_gateway.send(&enc_msg) {
-        Ok(n) => {
-            println!("Successfully sent HELLO message to server ({} bytes)", n);
-            Ok((encoded_gateway, decoded_gateway))
-        }
-        Err(_) => Err(std::io::Error::new(
-            std::io::ErrorKind::ConnectionAborted,
-            "Failed to send HELLO message",
-        )),
-    }
+    Ok((TcpStream::from_std(encoded_gateway), decoded_gateway))
 }
 
 fn prepare_server(
     encoded_addr: SocketAddr,
     decoded_addr: SocketAddr,
-    encoder: &Box<dyn Encoder>,
-) -> std::io::Result<(UdpSocket, UdpSocket)> {
+) -> std::io::Result<(TcpStream, UdpSocket)> {
     // In server mode, decoded gateway is connection to local server (connect to it).
     // Encoded gateway is connection to remote client (listen to it)
 
@@ -88,51 +73,21 @@ fn prepare_server(
     decoded_gateway.connect(decoded_addr)?;
 
     // Meanwhile encoded gateway just listens the specified port for incoming connections
-    let encoded_gateway = UdpSocket::bind(encoded_addr)?;
+    let encoded_listener = TcpListenerStd::bind(encoded_addr)?;
 
-    // Client will send HELLO message for immediate association
-    let mut buf = vec![0u8; 1024];
+    match encoded_listener.accept() {
+        Ok((encoded_gateway, _)) => {
+            println!("Successfully accepted connection from client");
+            return Ok((TcpStream::from_std(encoded_gateway), decoded_gateway))
+        }
 
-    // Message is blocking, therefore use busy waiting
-    let remote_addr: SocketAddr;
-    loop {
-        match encoded_gateway.recv_from(&mut buf) {
-            Ok((n, addr)) => {
-                buf = encoder.decode(buf[0..n].to_vec());
-                remote_addr = addr;
-                break;
-            }
-
-            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                // Do some busy waiting on non-blocking socket
-                thread::sleep(Duration::from_millis(100));
-                continue;
-            }
-
-            Err(_) => {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::ConnectionAborted,
-                    "Failed to receive HELLO message",
-                ));
-            }
+        Err(_) => {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::ConnectionAborted,
+                "Failed to establish connection with local client",
+            ));
         }
     }
-
-    if buf != "HELLO FROM YOKI CLIENT".as_bytes().to_vec() {
-        return Err(std::io::Error::new(
-            io::ErrorKind::InvalidData,
-            "HELLO message is corrupted",
-        ));
-    }
-
-    // Finally, associate encoded gateway with remote client
-    encoded_gateway.connect(remote_addr)?;
-    println!(
-        "Successfuly associated encoded gateway with {}",
-        remote_addr
-    );
-
-    Ok((encoded_gateway, decoded_gateway))
 }
 
 fn main() -> std::io::Result<()> {
@@ -166,19 +121,19 @@ fn main() -> std::io::Result<()> {
         .parse()
         .expect("Wrong format for decoded gateway");
 
-    let mut encoded_gateway: UdpSocket;
+    let mut encoded_gateway: TcpStream;
     let mut decoded_gateway: UdpSocket;
 
     match mode.as_str() {
         "client" => {
             (encoded_gateway, decoded_gateway) =
-                prepare_client(encoded_gateway_addr, decoded_gateway_addr, &encoder)
+                prepare_client(encoded_gateway_addr, decoded_gateway_addr)
                     .expect("Failed to initialize client");
         }
 
         "server" => {
             (encoded_gateway, decoded_gateway) =
-                prepare_server(encoded_gateway_addr, decoded_gateway_addr, &encoder)
+                prepare_server(encoded_gateway_addr, decoded_gateway_addr)
                     .expect("Failed to initialize server");
         }
 
@@ -211,7 +166,10 @@ fn main() -> std::io::Result<()> {
                     if let Ok((n, addr)) = decoded_gateway.recv_from(&mut buf) {
                         // Encode data and send to encoder client
                         decoded_counter += 1;
-                        println!("[{}] Received decoded message from {} ({} bytes)", decoded_counter, addr, n);
+                        println!(
+                            "[{}] Received decoded message from {} ({} bytes)",
+                            decoded_counter, addr, n
+                        );
                         buf = encoder.encode(buf[0..n].to_vec());
 
                         // do not transfer empty messages
@@ -220,7 +178,7 @@ fn main() -> std::io::Result<()> {
                         }
 
                         thread::sleep(Duration::from_millis(100));
-                        match encoded_gateway.send(&buf) {
+                        match encoded_gateway.write(&buf) {
                             Ok(m) => {
                                 println!(
                                     "Sent encoded message to {} ({} bytes)",
@@ -243,10 +201,13 @@ fn main() -> std::io::Result<()> {
                 Token(1) => {
                     // Encoded traffic comes here
 
-                    if let Ok((n, addr)) = encoded_gateway.recv_from(&mut buf) {
+                    if let Ok((n)) = encoded_gateway.read(&mut buf) {
                         // Decode data and send to decoder client
                         encoded_counter += 1;
-                        println!("[{}] Received encoded message from {} ({} bytes)", encoded_counter, addr, n);
+                        println!(
+                            "[{}] Received encoded message from ({} bytes)",
+                            encoded_counter, n
+                        );
                         buf = encoder.decode(buf[0..n].to_vec());
 
                         // do not transfer empty messages
